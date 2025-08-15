a8165c5cb2a5d59581f9646c55a1a9fec94ac834
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:39:01 -0300

    feat:tipagem e operacoes matematicas

---
+++ tipagem.py
@@ -0,0 +1,13 @@
+# Tipagem dinamica.
+
+# Limpar o terminal.+
+import os 
+os.system("cls")
+
+
+nome= input("Digite seu nome:")
+idade=input("Digite sua idade:")
+
+
+print(f"Nome:{nome}")
+print(f"idade:{idade}")
