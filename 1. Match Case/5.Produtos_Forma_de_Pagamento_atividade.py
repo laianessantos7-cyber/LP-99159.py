1cda845be9405333685891fface13967f5e368bb
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 16:16:33 -0300

    Exercicios.

---
+++ 1. Match Case/5.Produtos_Forma_de_Pagamento_atividade.py
@@ -0,0 +1,48 @@
+#Atividade 5- 03/09/2025
+
+
+#Limpar terminal.
+import os
+os.system("cls")
+
+
+
+produto= float(input("Digite o valor do produto: "))
+print("""
+1- Pagamento a vista
+2- Pagamento a prazo
+""")
+forma=int(input("Escolha a forma de pagamento( 1 ou 2): "))
+
+
+match forma:
+     case 1:
+        desconto= produto* 0.10
+        form_pagamento= 'valor a vista'
+        valor_total= produto - desconto
+        print(f"\nPagamento à vista selecionado.")
+        print(f"Valor do produto: R$ {produto:.2f}")
+        print(f"Desconto: R$ {desconto:.2f}")
+        print(f"Valor final: R$ {valor_total:.2f}")
+
+
+     case 2:
+         parcelas = int(input("Digite a quantidade de parcelas (até 6): "))
+         parcelas = 6
+         valor = produto /6
+         pagamento= "valor a prazo"
+         if parcelas >6 or parcelas <1:
+             print("Número de parcelas inválidos.")
+         else: 
+            print("Número de parcelas inválido. Usando 6 parcelas.")
+            parcelas = 6
+            valor_parcela = produto / parcelas
+            pagamento = "à prazo"
+            print(f"\nPagamento a prazo selecionado.")
+            print(f"Valor do produto: R$ {produto:.2f}")
+            print(f"Número de parcelas: {parcelas}")
+            print(f"Valor de cada parcela: R$ {valor_parcela:.2f}")
+            print(f"Valor total: R$ {produto:.2f}")
+
+
+     
