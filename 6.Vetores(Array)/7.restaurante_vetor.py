c46e2b904fa53410620749ddb7d2c399e5dae599
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-02 15:59:29 -0300

    feat: Atividades.

--- 6.Vetores(Array)/7.restaurante_vetor.py
+++ 6.Vetores(Array)/7.restaurante_vetor.py
@@ -2,56 +2,63 @@ import os
 os.system('cls')
 # Faça um algoritmo que mostre um menu com opções de um cardápio de restaurante para uma pessoa. A pessoa vai escolher o prato desejado. Após escolher o prato, o algoritmo deve perguntar ao usuário se ele deseja escolher outro prato.
 # Quando o usuário não quiser mais realizar pedidos, mostre o nome e o valor dos pratos escolhidos e calcule o valor  total da conta.
-lista_prato=[]
-lista_preco=[]
-total=[]
+
+
+
+lista_pratos = []
+precos_pratos = []
+
 
 
 print("=====CARDÁPIO====")
+while True:
+    opcao = int(input("""
+Código       Prato          Valor
+   1        Picanha         R$ 25,00
+   2        Lasanha         R$ 20,00
+   3        Strogonoff      R$ 18,00
+   4        Bife acebolado  R$ 15,00
+   5        Pão com ovo     R$ 5,00
+                     
+Digite a opção desejada: """))
+   
+    match opcao:
+        case 1:
+            prato = "Picanha"
+            preco = 25
+        case 2:
+            prato = "Lasanha"
+            preco = 20
+        case 3:
+            prato = "Strogonoff"
+            preco = 18
+        case 4:
+            prato = "Bife acebolado"
+            preco = 15
+        case 5:
+            prato = "Pão com ovo"
+            preco = 5
+        case _:
+            print("Opção inválida. \nTente novamente.\n")
+   
+    if opcao >= 1 and opcao <= 5:
+        lista_pratos.append(prato)
+        precos_pratos.append(preco)
 
-
-print("""
-Código \t Prato \t\t Valor
-1 \t Picanha \t R$25,00
-2 \t Lasanha \t R$20,00
-3 \t Strogonoff \t R$18,00
-4 \t Bife Acebolado\t R$15,00
-5 \t Pão com ovo \t R$ 5,00
-
-""")
-
-codigo= input("Digite o código de um prato: ")
-
-match codigo:
-    case "1":
-        print("Picanha: R$25,00.")
-        preco=25
-    case "2":
-        print("Lasanha: R$20,00.")
-        preco=20
-    case "3":
-        print("Strogonoff: R$18,00.")
-        preco=18
-    case "4":
-      print("Bife Acebolado: R$15,00.")
-      preco=15
-    case "5":
-        print("Pão com ovo: R$ 5,00 ")
-        preco=5
-
-    case _: 
-        print("Prato indisponível! ")
-
-    
-lista_prato.append
-lista_preco.append
-
-continuar = input("Deseja escolher outro prato? (s/n): ").lower()
-if continuar != "s":
- 
+    continuar = input("Deseja escolher outro prato? \nResponda com S ou N: ").lower()
+    if continuar == "n":
+        break
     os.system("cls")
 
+if sum(precos_pratos) == 0:
+    print("\nNenhum prato foi escolhido")
+else:
+    print("\n=PRATOS ESCOLHIDOS=")
+    for prato in lista_pratos:
+        print(f"Prato: {prato}")
 
-print("\nTotal a pagar: R${total: }")
-print("Obrigado pela preferência, Volte sempre!")
+    print(f"\nTotal: R$ {sum(precos_pratos):.2f}")
+
+
+print("\n Obrigado pela preferência, Volte sempre!")
 
