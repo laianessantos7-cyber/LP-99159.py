920d17189953f137381fba965f4a2517984598e3
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-03 16:06:21 -0300

    feat: Atividades.

--- 7.Funçoes/7.tabuada.py
+++ 7.Funçoes/7.tabuada.py
@@ -2,12 +2,16 @@ import os
 os.system('cls')
 
 
-def tabuada(numero):
+def tabuada(numero, operador):
     for i in range(1,11):
         print(f'{numero} x {i} = {numero * i }')
 
 
+
+
 numero=int(input('Digite um número: '))
+
+os.system('cls')
 tabuada(numero)
 
 print('==FIM==')
