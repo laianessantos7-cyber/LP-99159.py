0b09722e431f53b3bf882d44774d6e0307fba48d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-03 13:27:51 -0300

    feat: Atividades.

--- 6.Vetores(Array)/9.negativos_vetor.py
+++ 6.Vetores(Array)/9.negativos_vetor.py
@@ -4,8 +4,9 @@ os.system('cls')
 # Crie um algoritmo que receba do usuário valores e armazene em um vetor 5 números, caso seja informado um valor negativo, atribua o valor 0.
 # - Liste os valores do vetor.
 
+#Variaveis.
 
-listas=[]
+numeros=[] #- Lista.
 negativos= 0
 positivos= 0
 soma=0  
@@ -14,23 +15,23 @@ soma=0
 QUANTIDADE_NUMEROS=5
 
 #Inserindo notas:
-print(f'Solicitando {QUANTIDADE_NUMEROS} números')
+print(f'Solicitando {QUANTIDADE_NUMEROS} números:')
+
 for i in range(QUANTIDADE_NUMEROS):
-
-    numero=int(input(f'Digite o {i+1}ª número:'))
-    listas.append(numero)
-
-print('')
-for n1 in listas:
-    if i <0:
-        negativos+=1
+    numero = int(input(f'Digite o {i+1}º número: '))
+    
+    
+    if numero < 0:
+        numeros.append(0)
+        negativos += 1
     else:
-        positivos.append(negativos)
-
-    
+        numeros.append(numero)
+        positivos += 1
+        soma += numero
+#Mostrando.    
+print('\nNúmeros armazenados:', numeros)
 print(f"Quantidade de números positivos: {positivos}")
-print(f"Quantidade de números negativos: {negativos}")
-print(f'Soma dos positivos {sum(positivos)}')
-    
+print(f"Quantidade de números negativos informados: {negativos}")
+print(f"Soma dos valores positivos: {soma}")
+print("FIM")
 
-print('FIM')
